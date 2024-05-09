f87e0b8f1068ae67e0bf159ddd1a61a0d54ab8c7
Brimix <only.brimix@gmail.com>
2024-05-09 09:44:36 -0300

    Test capture

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print('Hello world!')
+from picamera2 import Picamera2, Preview
+from time import sleep
+
+camera = Picamera2()
+camera_config = camera.create_preview_configuration()
+
+camera.configure(camera_config)
+camera.start_preview(Preview.QTGL)
+camera.start()
+sleep(5)
+camera.capture_file("test.jpg")
