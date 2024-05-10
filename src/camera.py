bc9b148520962f053c3dbc318bccef206ce8b036
Brimix <only.brimix@gmail.com>
2024-05-10 11:36:51 -0300

    Test filters

--- src/camera.py
+++ src/camera.py
@@ -2,10 +2,11 @@ from picamera2 import Picamera2, Preview
 
 cam = Picamera2()
 
-def camera_init():
+def camera_init(debug):
     preview_config = cam.create_preview_configuration()
     cam.configure(preview_config)
-    cam.start_preview(Preview.QTGL)
+    if debug:
+        cam.start_preview(Preview.QTGL)
     cam.start()
 
 def camera_capture_array():
--- src/main.py
+++ src/main.py
@@ -2,27 +2,50 @@ import cv2
 import numpy as np
 import time
 from camera import camera_init, camera_stop, camera_capture_array
+from library import library_adjust, library_filter, library_segment, library_toGray
 
-def process_frame(label):
+def process_frame(label, blur_intensity):
     frame = camera_capture_array()
-    img = cv2.cvtColor(np.asarray(frame), cv2.COLOR_RGB2BGR)
-    filename = f"img/frame_{int(label)}.jpg"
-    cv2.imwrite(filename, img)
+    f = cv2.cvtColor(np.asarray(frame), cv2.COLOR_RGB2BGR)
+    f_gray = library_toGray(f)
+    f_just = library_adjust(f_gray)
+    f_blur = library_filter(f_just, blur_intensity)
+    f_can = library_segment(f_blur)
+
+    # Here goes the plot
+    # cv2.imshow("Gray", f_gray)
+    # cv2.imshow("Adjusted", f_just)
+    cv2.imshow("Blurred", f_blur)
+    cv2.imshow("Uncanny", f_can)
+
+    key = cv2.waitKey(1)
+    if key & 0xFF == ord('q'):
+        return False, blur_intensity
+    elif key & 0xFF == ord('w'):  # Up arrow key
+        blur_intensity += 0.1
+    elif key & 0xFF == ord('s'):  # Down arrow key
+        blur_intensity -= 0.1
+    return True, blur_intensity
 
 def main():
-    camera_init()
+    cam_debug = False
+    camera_init(cam_debug)
 
     last_time = time.time()
-    required_span = 1
+    required_span = 0.1
     it_number = 0
+    blur_intensity = 2.5
 
     try:
         while True:
             current_time = time.time()
             span_time = current_time - last_time
             if span_time >= required_span:
-                print('Process..')
-                process_frame(it_number)
+                # print('Process..')
+                result, blur_intensity = process_frame(it_number, blur_intensity)
+                print(blur_intensity)
+                if not result:
+                    break  # Exit the loop if process_frame returns False
                 it_number = it_number + 1
                 last_time = current_time
     except KeyboardInterrupt:
