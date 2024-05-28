612bd06eea1406b53ec6d108fc8031117a42c842
Brimix <only.brimix@gmail.com>
2024-05-28 14:01:56 -0300

    Add installation script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install python3-opencv
+sudo apt install python3-numpy
+sudo apt install python3-picamera2
+sudo apt install python3-RPi.GPIO
+sudo apt install python3-pigpio
+sudo apt install python3-transitions
