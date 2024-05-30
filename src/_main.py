8789a33367e09070c78478cdc888b16a011fa71d
Brimix <only.brimix@gmail.com>
2024-05-30 05:20:21 -0300

    Add debug stuff

--- src/_main.py
+++ src/_main.py
@@ -3,41 +3,43 @@ import numpy as np
 import time
 import keystrokes
 import _parameters as param
+import view
 
 import image_processing.process as imp
 import image_processing.filter_parameters as im_param
-import image_processing.debug_tools as im_dbg
+# import image_processing.debug_tools as im_dbg
 
 import computer_vision.functions as cvf
 from cnn.predictor import predict_from_image
 
 def process_frame():
-    # obj = cvf.get_object()
+    imp.get_filtered_frame()
     # if (obj is not None):
     #     color, shape = obj
-    #     print('--- ', color, shape)
+        # print('--- ', color, shape)
     # else:
-    #     print('. Nothing to show')
-    img = cvf.create_image()
-    result = predict_from_image(img)
-    if (img is None):
-        print('Efe')
-        return
-    cv2.imshow('Coso', img)
-    print('Eso', result)
-    cv2.waitKey(1)
+        # print('. Nothing to show')
+    # img = cvf.create_image()
+    # result = predict_from_image(img)
+    # if (img is None):
+    #     print('Efe')
+    #     return
+    # cv2.imshow('Coso', img)
+    # print('Eso', result)
+    # cv2.waitKey(1)
 
 
 def main():
     imp.init()
     keystrokes.init()
+    view.create_window()
 
     last_time = time.time()
     try:
         while True:
             key = keystrokes.get_stroke()
             im_param.tweak_by_key(key)
-            im_dbg.switch_step()
+            # im_dbg.switch_step()
 
             param.tweak_by_key(key)
             current_time = time.time()
---
+++ src/coso.py
@@ -0,0 +1,6 @@
+from units import carrier, train
+import time
+
+while True:
+    train.set_position_zero()
+    time.sleep(5)
